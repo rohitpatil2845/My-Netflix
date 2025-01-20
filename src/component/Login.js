090bc6bf81ae1cd6d1c8a763b0a3745e9b7a4af3
Rohit Patil <rohitpatil200528@gmail.com>
2025-01-20 19:51:53 +0530

    Sign In And Sign Up page

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
-    const toggleSignForm = () => {
-        
-    }
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -13,11 +16,18 @@ const Login = () => {
           className="absolute"
           alt="background-img"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_large.jpg"
-          srcset="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_large.jpg 1800w"
+          srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_large.jpg 1800w"
         />
       </div>
-      <form className="absolute w-2/6 p-8 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-75">
-        <p className="font-bold text-3xl p-4">Sign In</p>
+      <form className="absolute rounded-2xl w-96 h-96 p-8 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-75">
+        <h1 className="font-bold text-3xl p-2">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+       {!isSignInForm && <input
+          type="text"
+          placeholder="Name"
+          className="p-2 m-2 rounded-lg  bg-zinc-600 w-full"
+        />}
         <input
           type="text"
           placeholder="Email"
@@ -29,9 +39,15 @@ const Login = () => {
           className="p-2 m-2 rounded-lg  bg-zinc-600 w-full"
         />
         <button className="p-2 m-2 rounded-lg bg-red-700 w-full">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4 " onClick={toggleSignForm()}>New to Netflix? Sign Up</p>
+        <button
+          className="py-2"
+          type="button"
+          onClick={toggleSignForm}
+        >
+          {isSignInForm ? "New to Netflix? Sign Up" : "Already have an account? Sign In"}
+        </button>
       </form>
     </div>
   );
